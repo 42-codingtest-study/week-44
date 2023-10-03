435906b8b3423e9f4c8e1075d6d6730e5fa80c66
Chanwoong1 <jcw7468@gmail.com>
2023-10-03 16:32:00 +0900

    주석 추가

--- Doit_031.cpp
+++ Doit_031.cpp
@@ -15,16 +15,20 @@ int N, k, s, e, m, tmp, answer;
 
 int main() {
   fast;
-  cin >> N >> k;
-  s = 1, e = k;
+  cin >> N >> k;  // 행렬의 크기와 K 입력
+  s = 1, e = k;   // 이분 탐색 범위 초기화
+  // 이분 탐색 시작
   while (s <= e) {
-    m = (s + e) / 2;
-    tmp = 0;
+    m = (s + e) / 2, tmp = 0;
+    // 1부터 N까지 각 행에서 m보다 작거나 같은 수의 개수를 계산
     for (int i = 1; i <= N; i++) tmp += min(m / i, N);
+
+    // 계산된 tmp가 k 이상이면 답 후보 갱신 및 탐색 범위 변경
     if (tmp >= k)
       answer = m, e = m - 1;
     else
       s = m + 1;
   }
-  cout << s;
+
+  cout << s;  // K번째 수 출력
 }
